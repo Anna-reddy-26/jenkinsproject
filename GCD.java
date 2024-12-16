f23c736fc40ef65ed868e92cb8dfde4d32588d81
annareddy <pradeepreddygeetla@gmail.com>
2024-12-16 15:44:58 +0530

    added a new item

--- GCD.java
+++ GCD.java
@@ -9,7 +9,7 @@ public class GCD{
 	public static void main(String[]args){
 		int num 1=10;
 		int num 2=20;
-		int gcd=find GCD(num 1,num 2)
+		int gcd=find GCD(num 1,num 2);
 			System.out.println("the GCD of",+num1+"and" +num2+"is",+gcd);
 	}
 }
